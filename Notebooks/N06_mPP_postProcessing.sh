f5af29f7c1eb8fc4ee61e672970283bfbe24ae90
javiergcas <javiergcas@gmail.com>
2022-03-03 17:47:32 -0500

    N06 updated to create Reference, signals per region for all pipelines, and correlation maps for all pipelines

--- Notebooks/N06_mPP_postProcessing.sh
+++ Notebooks/N06_mPP_postProcessing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Author: Javier Gonzalez-Castillo
-# Data: 24th August, 2021
+# Data: 3th March, 2022
 #
 # Notes:
 # ======
@@ -71,13 +71,28 @@ else
    echo "++ WARNING: Blur.Scale file already exists. Not generating it again."
 fi
 
+# (4) Extract Representative Timseries for scale.blur 
+# -----------------------------------------------------
+echo "++ INFO: (4) Extract Representative Timseries"
+echo "==============================================="
+for items in ${ROI_FV_PATH},'V4_grp' ${ROI_FVlt_PATH},'V4lt_grp' ${ROI_FVut_PATH},'V4ut_grp' ../ROI.V4_e.mPP.nii.gz,'V4_e' ../ROI.Vl_e.mPP.nii.gz,'Vl_e' ../ROI.GM.mPP.nii.gz,'GM' ../ROI.FB.mPP.nii.gz,'FB' ../ROI.WM_e.mPP.nii.gz,'WM_e'
+do 
+   IFS="," ; set -- $items
+   echo " + INFO: Mask = $1 | Suffix = $2 --> Output = ${RUN}_BASIC.Signal.$2.1D"
+   3dmaskave -quiet -mask $1 ${RUN}_mPP.blur.scale.nii.gz   > rm.aux.$2.1D
+   3dDetrend -prefix - -polort ${POLORT} rm.aux.$2.1D\'  > rm.det.aux.$2.1D
+   cat rm.det.aux.$2.1D | tr -s ' ' '\n' | sed '/^$/d' > ${RUN}_mPP.blur.scale.Signal.$2.1D
+   rm rm.aux.$2.1D rm.det.aux.$2.1D
+   1d_tool.py -overwrite -infile ${RUN}_mPP.blur.scale.Signal.$2.1D -derivative -write ${RUN}_mPP.blur.scale.Signal.$2.der.1D
+done
+
 # ================================================================================
 # =============================== PART 1: QA Metrics =============================
 # ================================================================================
 
-# (4) Compute DVARS (SRMS) in minimally pre-processed data
+# (5) Compute DVARS (SRMS) in minimally pre-processed data
 # ====================================================
-echo "++ INFO: (4) Computing DVARS (SRMS) in minimally pre-processed dataset"
+echo "++ INFO: (5) Computing DVARS (SRMS) in minimally pre-processed dataset"
 echo "++ ==================================================================="
 3dTto1D -overwrite                                   \
         -input ${RUN}_mPP.nii.gz[${VOLS_DISCARD}..$] \
@@ -93,31 +108,31 @@ tail -n ${nt} ${RUN}_Movement_Regressors_dt.txt >  ${RUN}_Movement_Regressors_dt
 # =============================== PART 2: Generate Regressors of interest =========
 # =================================================================================
 
-# (5) Generate Bandpass filtering regressors
+# (6) Generate Bandpass filtering regressors
 # ======================================
-echo "++ INFO: (5) Generating bandpass filtering regressors"
+echo "++ INFO: (6) Generating bandpass filtering regressors"
 echo "++ =================================================="
 1dBport -nodata ${nt} ${tr} -band 0.01 0.1   -invert -nozero > ${RUN}_Bandpass_Regressors.txt
 
-echo "++ INFO: (6) Generating Behzadi-style CompCorr regressors"
-echo "++ ======================================================"
-# (6) Generate CompCorr regressors (Behzadi Style)
+# (7) Generate CompCorr regressors (Behzadi Style)
 # ================================================
-# 6.1 We extract PCA from detrended dataset (to match later regression)
+echo "++ INFO: (7) Generating Behzadi-style CompCorr regressors"
+echo "++ ======================================================"
+# 7.1 We extract PCA from detrended dataset (to match later regression)
 3dTproject -overwrite \
             -mask ../ROI.FB.mPP.nii.gz \
             -polort ${POLORT} \
             -prefix rm.det_pcin_${RUN}.nii.gz \
             -input ${RUN}_mPP.nii.gz[${VOLS_DISCARD}..$]
 
-# 6.2 Extract PCA from detrended dataset (to match later regression)
+# 7.2 Extract PCA from detrended dataset (to match later regression)
 3dpc -overwrite                                        \
      -mask ../ROI.compcorr.mPP.nii.gz                      \
      -pcsave 5                                         \
      -prefix ${RUN}_mPP_Behzadi_CompCorr_Regressors \
      rm.det_pcin_${RUN}.nii.gz
 
-# 6.3 Remove redundant files
+# 7.3 Remove redundant files
 rm ${RUN}_mPP_Behzadi_CompCorr_Regressors+tlrc.????
 rm ${RUN}_mPP_Behzadi_CompCorr_Regressors0?.1D
 rm ${RUN}_mPP_Behzadi_CompCorr_Regressors_eig.1D
@@ -127,9 +142,42 @@ rm rm.det_pcin_${RUN}.nii.gz
 # ======================================================================================== #
 # ================ Run different pre-processing pipelines (post mPP) ===================== #
 # ======================================================================================== #
-# (7) Basic Pre-processing 
+
+# (8) Create Reference for comparison of denoising strategies
+# ===========================================================
+echo "++ INFO: (8) Reference Pre-processing"
+echo "++ =================================="
+3dTproject -overwrite                                          \
+           -mask   ../ROI.FB.mPP.nii.gz                        \
+           -input  ${RUN}_mPP.blur.scale.nii.gz                \
+           -polort ${POLORT}                                   \
+           -ort    ${RUN}_Bandpass_Regressors.txt              \
+           -prefix ${RUN}_Reference.nii.gz
+echo " + OUTPUT from Basic Pipeline: ${RUN}_Reference.nii.gz"
+
+# (8.1) Extract Representative Timeseries for Reference
+echo "++ INFO: (8.1) Extract Representative Timseries for Reference"
+echo "============================================================="
+for items in ${ROI_FV_PATH},'V4_grp' ${ROI_FVlt_PATH},'V4lt_grp' ${ROI_FVut_PATH},'V4ut_grp' ../ROI.V4_e.mPP.nii.gz,'V4_e' ../ROI.Vl_e.mPP.nii.gz,'Vl_e' ../ROI.GM.mPP.nii.gz,'GM' ../ROI.FB.mPP.nii.gz,'FB' ../ROI.WM_e.mPP.nii.gz,'WM_e'
+do 
+   IFS="," ; set -- $items
+   echo " + INFO: Mask = $1 | Suffix = $2 --> Output = ${RUN}_BASIC.Signal.$2.1D"
+   3dmaskave -quiet -mask $1 ${RUN}_Reference.nii.gz   > rm.aux.$2.1D
+   3dDetrend -prefix - -polort ${POLORT} rm.aux.$2.1D\'  > rm.det.aux.$2.1D
+   cat rm.det.aux.$2.1D | tr -s ' ' '\n' | sed '/^$/d' > ${RUN}_mPP.blur.scale.Signal.$2.1D
+   rm rm.aux.$2.1D rm.det.aux.$2.1D
+   1d_tool.py -overwrite -infile ${RUN}_Reference.Signal.$2.1D -derivative -write ${RUN}_Reference.Signal.$2.der.1D
+done
+ 
+# (8.2) Compute variance for Reference
+echo "++ INFO (8.2) Compute variance for output of Reference pipeline"
+echo "==============================================================="
+3dTstat -overwrite -stdev -prefix ${RUN}_Reference.VAR.nii.gz ${RUN}_Reference.nii.gz
+3dcalc  -overwrite -a ${RUN}_Reference.VAR.nii.gz -expr 'a*a' -prefix ${RUN}_Reference.VAR.nii.gz 
+
+# (9) Basic Pre-processing 
 # ========================
-echo "++ INFO: (7) Basic Pre-processing"
+echo "++ INFO: (9) Basic Pre-processing"
 echo "++ =============================="
 3dTproject -overwrite                                          \
            -mask   ../ROI.FB.mPP.nii.gz                        \
@@ -140,9 +188,29 @@ echo "++ =============================="
            -prefix ${RUN}_BASIC.nii.gz
 echo " + OUTPUT from Basic Pipeline: ${RUN}_BASIC.nii.gz"
 
-# (8) Basic Pre-processing - No Filtering
+# (9.1) Extract Representative Timseries for BASIC
+echo "++ INFO: (9.1) Extract Representative Timseries"
+echo "==============================================="
+for items in ${ROI_FV_PATH},'V4_grp' ${ROI_FVlt_PATH},'V4lt_grp' ${ROI_FVut_PATH},'V4ut_grp' ../ROI.V4_e.mPP.nii.gz,'V4_e' ../ROI.Vl_e.mPP.nii.gz,'Vl_e' ../ROI.GM.mPP.nii.gz,'GM' ../ROI.FB.mPP.nii.gz,'FB' ../ROI.WM_e.mPP.nii.gz,'WM_e'
+do 
+   IFS="," ; set -- $items
+   echo " + INFO: Mask = $1 | Suffix = $2 --> Output = ${RUN}_BASIC.Signal.$2.1D"
+   3dmaskave -quiet -mask $1 ${RUN}_BASIC.nii.gz   > rm.aux.$2.1D
+   3dDetrend -prefix - -polort ${POLORT} rm.aux.$2.1D\'  > rm.det.aux.$2.1D
+   cat rm.det.aux.$2.1D | tr -s ' ' '\n' | sed '/^$/d' > ${RUN}_BASIC.Signal.$2.1D
+   rm rm.aux.$2.1D rm.det.aux.$2.1D
+   1d_tool.py -overwrite -infile ${RUN}_BASIC.Signal.$2.1D -derivative -write ${RUN}_BASIC.Signal.$2.der.1D
+done
+
+# (9.2) Compute variance for BASIC
+echo "++ INFO (9.2) Compute variance for output of BASIC pipeline"
+echo "==========================================================="
+3dTstat -overwrite -stdev -prefix ${RUN}_BASIC.VAR.nii.gz ${RUN}_BASIC.nii.gz
+3dcalc  -overwrite -a ${RUN}_BASIC.VAR.nii.gz -expr 'a*a' -prefix ${RUN}_BASIC.VAR.nii.gz 
+
+# (10) Basic Pre-processing - No Filtering
 # =========================================
-echo "++ INFO: (8) Basic Pre-processing (no filtering - input to rapidtide)"
+echo "++ INFO: (10) Basic Pre-processing (no filtering - input to rapidtide)"
 echo "++ =================================================================="
 3dTproject -overwrite                                          \
            -mask   ../ROI.FB.mPP.nii.gz                        \
@@ -152,9 +220,9 @@ echo "++ =================================================================="
            -prefix ${RUN}_BASICnobpf.nii.gz
 echo " + OUTPUT from Basic Pipeline (no filtering): ${RUN}_BASICnobpf.nii.gz"
 
-# (9) Behzadi CompCor Pipeline
+# (11) Behzadi CompCor Pipeline
 # =============================
-echo "++ INFO: (9) Behzadi CompCorr Pre-processing"
+echo "++ INFO: (11) Behzadi CompCorr Pre-processing"
 echo "++ =========================================="
 3dTproject -overwrite                                           \
            -mask   ../ROI.FB.mPP.nii.gz                         \
@@ -165,6 +233,31 @@ echo "++ =========================================="
            -ort    ${RUN}_mPP_Behzadi_CompCorr_Regressors_vec.1D        \
            -prefix ${RUN}_Behzadi_COMPCOR.nii.gz
 echo " + OUTPUT from Behzadi CompCor: ${RUN}_Behzadi_COMPCOR.nii.gz"
+
+# (11.1) Extract Representative Timseries for BASIC
+echo "++ INFO: (11.1) Extract Representative Timseries"
+echo "================================================"
+for items in ${ROI_FV_PATH},'V4_grp' ${ROI_FVlt_PATH},'V4lt_grp' ${ROI_FVut_PATH},'V4ut_grp' ../ROI.V4_e.mPP.nii.gz,'V4_e' ../ROI.Vl_e.mPP.nii.gz,'Vl_e' ../ROI.GM.mPP.nii.gz,'GM' ../ROI.FB.mPP.nii.gz,'FB' ../ROI.WM_e.mPP.nii.gz,'WM_e'
+do 
+  IFS="," ; set -- $items
+   echo " + INFO: Mask = $1 | Suffix = $2 --> Output = ${RUN}_Behzadi_COMPCOR.Signal.$2.1D"
+   3dmaskave -quiet -mask $1 ${RUN}_Behzadi_COMPCOR.nii.gz   > rm.aux.$2.1D
+   3dDetrend -prefix - -polort ${POLORT} rm.aux.$2.1D\'  > rm.det.aux.$2.1D
+   cat rm.det.aux.$2.1D | tr -s ' ' '\n' | sed '/^$/d' > ${RUN}_Behzadi_COMPCOR.Signal.$2.1D
+   rm rm.aux.$2.1D rm.det.aux.$2.1D
+   1d_tool.py -overwrite -infile ${RUN}_Behzadi_COMPCOR.Signal.$2.1D -derivative -write ${RUN}_Behzadi_COMPCOR.Signal.$2.der.1D
+done
+
+# 11.2 Compute variance for COMPCOR
+echo "++ INFO (11.2) Compute variance for output of COMPCOR pipeline"
+echo "=============================================================="
+3dTstat -overwrite -stdev -prefix ${RUN}_COMPCOR.VAR.nii.gz ${RUN}_Behzadi_COMPCOR.nii.gz
+3dcalc  -overwrite -a ${RUN}_COMPCOR.VAR.nii.gz -expr 'a*a' -prefix ${RUN}_COMPCOR.VAR.nii.gz 
+
+echo "++ INFO: (12) Maps of correlation between the different pre-processing"
+echo "++ ==================================================================="
+3dTcorrelate -overwrite -prefix ${RUN}_BASIC_2_Reference.nii.gz   ${RUN}_BASIC.nii.gz ${RUN}_Reference.nii.gz
+3dTcorrelate -overwrite -prefix ${RUN}_COMPCOR_2_Reference.nii.gz ${RUN}_Behzadi_COMPCOR.nii.gz ${RUN}_Reference.nii.gz
 
 echo "=================================="
 echo "++ INFO: Script finished correctly"
